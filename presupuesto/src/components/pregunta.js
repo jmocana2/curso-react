392897a0ada9589c5d5f34cffe2015a0ef2e6aad
jose.ocana <jose.ocana@i3television.es>
2021-10-07 17:04:04 +0200

    agregamos estado cantidad

--- presupuesto/src/components/pregunta.js
+++ presupuesto/src/components/pregunta.js
@@ -1,15 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Pregunta = () => {
+  
+  const [cantidad, guardarCantidad] = useState(0);
+
+  //función que lee el presupuesto
+  const definirPresupuesto = (e) => {
+    guardarCantidad(parseInt(e.target.value, 10));
+  }
+
+  //Submit para definir el presupuesto
+  const agregarPresupuesto = e => {
+    e.preventDefault();
+
+    // validación
+
+
+    // pasamos la validación
+  }
+    
   return ( 
     <>
       <h2>Coloca tu presupuesto</h2>
-      <form>
+      <form onSubmit={e=>agregarPresupuesto(e)}>
           <input
               type="number"
               className="u-full-width"
               placeholder="Coloca tu presupuesto"
-            
+              onChange={e=>definirPresupuesto(e)}
           />
           <input 
               type="submit"
