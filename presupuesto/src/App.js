63cf607f3999a2a66ae33f143df5fd5cadf3f8d9
jose.ocana <jose.ocana@i3television.es>
2021-10-11 10:00:03 +0200

    Cargando un componente u otro

--- presupuesto/src/App.js
+++ presupuesto/src/App.js
@@ -1,19 +1,41 @@
 import React, { useState } from 'react';
-
+import Formulario from './components/Formulario';
 import Pregunta from "./components/Pregunta";
 
 function App() {
 
   const [presupuesto, guardarPresupuesto] = useState(0);
   const [restante, guardarRestante] = useState(0);
+  const [mostrarPregunta, actualizarPregunta] = useState(true);
   
   return (
     <div className="container">
       <header>
         <h1>Gasto semanal</h1>
         <div className="contenido-principal contenido">
-          <Pregunta guardarPresupuesto={guardarPresupuesto} guardarRestante={guardarRestante} />
+          {mostrarPregunta ?
+          (
+            <Pregunta 
+              guardarPresupuesto={guardarPresupuesto} 
+              guardarRestante={guardarRestante} 
+              actualizarPregunta={actualizarPregunta}  />
+          )
+          :
+          (
+            <div className="row">
+              <div className="one-half column">
+                  <Formulario 
+                  
+                  />
+              </div>
+
+              <div className="one-half column">
+                
+              </div>
+            </div>
+          )}
         </div>
+        
       </header>
     </div>
   );
---
+++ presupuesto/src/components/Formulario.js
@@ -0,0 +1,46 @@
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+import Error from './Error';
+
+const Formulario = ({guardarGasto, guardarCrearGasto}) => {
+ 
+    return ( 
+        <form
+        
+        >
+            <h2>Agrega tus gastos aquí</h2>
+
+                  <div className="campo">
+                <label>Nombre Gasto</label>
+                <input 
+                    type="text"
+                    className="u-full-width"
+                    placeholder="Ej. Transporte"               
+                />
+            </div>
+
+            <div className="campo">
+                <label>Cantidad Gasto</label>
+                <input 
+                    type="number"
+                    className="u-full-width"
+                    placeholder="Ej. 300"                 
+                />
+            </div>
+
+            <input
+                type="submit"
+                className="button-primary u-full-width"
+                value="Agregar Gasto"
+            />
+        </form>
+
+     );
+}
+
+Formulario.propTypes = {
+    guardarGasto: PropTypes.func.isRequired,
+    guardarCrearGasto: PropTypes.func.isRequired
+}
+ 
+export default Formulario;
