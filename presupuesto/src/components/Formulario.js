7e00be13278180fabbedc3ce8e944bab7bde0049
jose.ocana <jose.ocana@i3television.es>
2021-10-11 12:48:28 +0200

    Creando el State para los gastos

--- presupuesto/src/components/Formulario.js
+++ presupuesto/src/components/Formulario.js
@@ -4,38 +4,58 @@ import Error from './Error';
 
 const Formulario = ({guardarGasto, guardarCrearGasto}) => {
  
-    return ( 
-        <form
-        
-        >
-            <h2>Agrega tus gastos aquí</h2>
+  const [nombregasto, agregarNombreGasto] = useState('');
+  const [cantidad, guardarCantidad] = useState(0);
 
-                  <div className="campo">
-                <label>Nombre Gasto</label>
-                <input 
-                    type="text"
-                    className="u-full-width"
-                    placeholder="Ej. Transporte"               
-                />
-            </div>
+  // agregar nuevo gasto
+  const agregarGasto = e => {
+    e.preventDefault();
 
-            <div className="campo">
-                <label>Cantidad Gasto</label>
-                <input 
-                    type="number"
-                    className="u-full-width"
-                    placeholder="Ej. 300"                 
-                />
-            </div>
+    // validar
 
-            <input
-                type="submit"
-                className="button-primary u-full-width"
-                value="Agregar Gasto"
-            />
-        </form>
+    // construir el gasto
 
-     );
+    // pasar el gasto al componente principal
+
+    // resetear form
+  }
+
+  return ( 
+    <form
+      onSubmit={agregarGasto}
+    >
+        <h2>Agrega tus gastos aquí</h2>
+
+        <div className="campo">
+          <label>Nombre Gasto</label>
+          <input 
+            type="text"
+            className="u-full-width"
+            placeholder="Ej. Transporte"
+            value={nombregasto}
+            onChange={e => agregarNombreGasto(e.target.value)}
+          />
+        </div>
+
+        <div className="campo">
+          <label>Cantidad Gasto</label>
+          <input 
+            type="number"
+            className="u-full-width"
+            placeholder="Ej. 300"
+            value={cantidad}
+            onChange={e => guardarCantidad(parseInt(e.target.value, 10))}
+          />
+        </div>
+
+        <input
+          type="submit"
+          className="button-primary u-full-width"
+          value="Agregar Gasto"
+        />
+    </form>
+
+  );
 }
 
 Formulario.propTypes = {
