a55cc8ec21daa716a2b65861d8b1c37b19348c2e
jose.ocana <jose.ocana@i3television.es>
2021-10-14 16:17:05 +0200

    Validaciones

--- cotizador/src/components/Formulario.js
+++ cotizador/src/components/Formulario.js
@@ -56,6 +56,7 @@ const Formulario = () => {
       year: '',
       plan: ''
     });
+    const [error, guardarError] = useState(false);
 
     // Extraer los valores del state
     const { marca, year, plan } = datos;
@@ -67,70 +68,79 @@ const Formulario = () => {
         [e.target.name] : e.target.value
       })
     }
+
+    // validar formulario
+    const cotizarSeguro = (e) => {
+      e.preventDefault();
+      if(marca === '' || year === '' || plan === ''){
+        guardarError(true);
+        return;
+      }
+
+      guardarError(false);
+    }
     
     return ( 
-        <form
-         
+      <form
+         onSubmit={cotizarSeguro}
         >
-          
+        {error ? <Error>Todos los campos son obligatorios</Error> : null}
+        <Campo>
+            <Label>Marca</Label>
+            <Select
+                name="marca"
+                value={marca}
+                onChange={obtenerInformacion}
+            >
+                <option value="">-- Seleccione --</option>
+                <option value="americano">Americano</option>
+                <option value="europeo">Europeo</option>
+                <option value="asiatico">Asiatico</option>
+            </Select>
+        </Campo>
 
-            <Campo>
-                <Label>Marca</Label>
-                <Select
-                    name="marca"
-                    value={marca}
-                    onChange={obtenerInformacion}
-                >
-                    <option value="">-- Seleccione --</option>
-                    <option value="americano">Americano</option>
-                    <option value="europeo">Europeo</option>
-                    <option value="asiatico">Asiatico</option>
-                </Select>
-            </Campo>
+        <Campo>
+            <Label>Año</Label>
+            <Select
+                name="year"
+                value={year}
+                onChange={obtenerInformacion}
+            >
+                <option value="">-- Seleccione --</option>
+                <option value="2021">2021</option>
+                <option value="2020">2020</option>
+                <option value="2019">2019</option>
+                <option value="2018">2018</option>
+                <option value="2017">2017</option>
+                <option value="2016">2016</option>
+                <option value="2015">2015</option>
+                <option value="2014">2014</option>
+                <option value="2013">2013</option>
+                <option value="2012">2012</option>
+            </Select>
+        </Campo>
 
-            <Campo>
-                <Label>Año</Label>
-                <Select
-                    name="year"
-                    value={year}
-                    onChange={obtenerInformacion}
-                >
-                    <option value="">-- Seleccione --</option>
-                    <option value="2021">2021</option>
-                    <option value="2020">2020</option>
-                    <option value="2019">2019</option>
-                    <option value="2018">2018</option>
-                    <option value="2017">2017</option>
-                    <option value="2016">2016</option>
-                    <option value="2015">2015</option>
-                    <option value="2014">2014</option>
-                    <option value="2013">2013</option>
-                    <option value="2012">2012</option>
-                </Select>
-            </Campo>
+        <Campo>
+            <Label>Plan</Label>
+            <InputRadio 
+                type="radio"
+                name="plan"
+                value="basico"
+                checked={plan === 'basico'}
+                onChange={obtenerInformacion}
+            /> Básico
 
-            <Campo>
-                <Label>Plan</Label>
-                <InputRadio 
-                    type="radio"
-                    name="plan"
-                    value="basico"
-                    checked={plan === 'basico'}
-                    onChange={obtenerInformacion}
-                /> Básico
+            <InputRadio 
+                type="radio"
+                name="plan"
+                value="completo"
+                checked={plan === 'completo'}
+                onChange={obtenerInformacion}
+            /> Completo
+        </Campo>
 
-                <InputRadio 
-                    type="radio"
-                    name="plan"
-                    value="completo"
-                    checked={plan === 'completo'}
-                    onChange={obtenerInformacion}
-                /> Completo
-            </Campo>
-
-            <Boton type="submit">Cotizar</Boton>
-        </form>
-
+        <Boton type="submit">Cotizar</Boton>
+      </form>
      );
 }
 
