79f9e4ee71aa5dc1c27dda72f27d177d924143d9
jose.ocana <jose.ocana@i3television.es>
2021-10-20 17:12:38 +0200

    Creando formulario

--- clima/src/App.js
+++ clima/src/App.js
@@ -1,9 +1,24 @@
 import React from 'react';
+import Formulario from './components/Formulario';
 import Header from "./components/Header";
 
 function App() {
   return (
-    <Header titulo="Clima React" />
+    <>
+      <Header titulo="Clima React" />
+      <div className="contenedor-form">
+        <div className="container">
+          <div className="row">
+            <div className="col m6 s12">
+              <Formulario />
+            </div>
+            <div className="col m6 s12">
+            
+            </div>
+          </div>
+        </div>
+      </div>
+    </>  
   );
 }
 
