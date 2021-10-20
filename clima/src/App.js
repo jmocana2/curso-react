728a7762aea7db9dccee25c86f1d3fd5ed6f1040
jose.ocana <jose.ocana@i3television.es>
2021-10-20 18:11:41 +0200

    Obteniendo resultados de la API

--- clima/src/App.js
+++ clima/src/App.js
@@ -1,49 +1,61 @@
-import React, { useState, useEffect } from 'react';
+import React, { Fragment, useState, useEffect } from 'react';
+import Header from './components/Header';
 import Formulario from './components/Formulario';
-import Header from "./components/Header";
 
 function App() {
 
-  //state
-  const [busqueda, guardarBusqueda] = useState({
-    ciudad: '',
-    pais: ''
+    // state del formulario
+  const [busqueda, guardarBusqueda] = useState({ 
+      ciudad: '',
+      pais: ''
   });
   const [consultar, guardarConsultar] = useState(false);
+  const [resultado, guardarResultado] = useState({});
 
   const { ciudad, pais } = busqueda;
 
   useEffect(() => {
-    console.log('peticion api...');
-
     const consultarAPI = async () => {
-      await fetch('api url');
+
+        if(consultar) {
+          const appId = '142f1f851f2fa324e563d088fbd4cbba';
+          const url = `http://api.openweathermap.org/data/2.5/weather?q=${ciudad},${pais}&appid=${appId}`;
+  
+          const respuesta = await fetch(url);
+          const resultado = await respuesta.json();
+  
+          guardarResultado(resultado);
+          guardarConsultar(false);
+        }
+        
     }
     consultarAPI();
-
-
-  }, [consultar]);
-
-  
-
-
+    // eslint-disable-next-line
+  },[consultar]);
 
   return (
-    <>
-      <Header titulo="Clima React" />
-      <div className="contenedor-form">
-        <div className="container">
-          <div className="row">
-            <div className="col m6 s12">
-              <Formulario busqueda={busqueda} guardarBusqueda={guardarBusqueda} guardarConsultar={guardarConsultar} />
+    <Fragment>
+        <Header 
+          titulo='Clima React App'
+        />
+
+        <div className="contenedor-form">
+            <div className="container">
+                <div className="row">
+                    <div className="col m6 s12">
+                        <Formulario 
+                          busqueda={busqueda}
+                          guardarBusqueda={guardarBusqueda}
+                          guardarConsultar={guardarConsultar}
+                        />
+                    </div>
+                    <div className="col m6 s12">
+                    
+                    </div>
+                </div>
             </div>
-            <div className="col m6 s12">
-            
-            </div>
-          </div>
         </div>
-      </div>
-    </>  
+    </Fragment>
   );
 }
 
