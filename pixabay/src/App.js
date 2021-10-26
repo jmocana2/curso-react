7fd2816e74be06201fae6b497e77f75ef9d796d7
jose.ocana <jose.ocana@i3television.es>
2021-10-26 18:24:40 +0200

    Añadiendo páginación y finalizando proyecto

--- pixabay/src/App.js
+++ pixabay/src/App.js
@@ -7,6 +7,22 @@ function App() {
   //definimos state
   const [busqueda, guardarBusqueda] = useState('');
   const [imagenes, guardarImagenes] = useState([]);
+  const [paginaactual, guardarPaginaActual ] = useState(1);
+  const [totalpaginas, guardarTotalPaginas] = useState(1);
+
+  const paginaAnterior = () => {
+    if(paginaactual === 1) return
+
+    const nuevaPaginaActual = paginaactual - 1;
+    guardarPaginaActual(nuevaPaginaActual);
+  }
+
+  const paginaSiguiente = () => {
+    if(paginaactual === totalpaginas + 1) return
+
+    const nuevaPaginaActual = paginaactual + 1;
+    guardarPaginaActual(nuevaPaginaActual);
+  }
 
   useEffect(() => {
 
@@ -14,44 +30,52 @@ function App() {
 
     const consultarAPI = async() => {
       const fotosPagina = 10;
-      const url = `https://pixabay.com/api/?key=543056-4ef46f61f7481b21974e39be4&q=${busqueda}&per_page=${fotosPagina}`;
+      const url = `https://pixabay.com/api/?key=543056-4ef46f61f7481b21974e39be4&q=${busqueda}&per_page=${fotosPagina}&page=${paginaactual}`;
       const consulta = await fetch(url);
       const resultado = await consulta.json();
 
       console.log(resultado);
-      guardarImagenes(resultado.hits);
+      guardarImagenes(resultado.hits);  
+      guardarTotalPaginas(Math.ceil(resultado.totalHits / fotosPagina));
+
+      const jumbotron = document.querySelector('.jumbotron');
+      jumbotron.scrollIntoView({behavior: 'smooth'});
     }
     consultarAPI();
-  }, [busqueda])
+  }, [busqueda, paginaactual])
 
   return (
     <div className="container">
       <div className="jumbotron">
           <p className="lead text-center">Buscador de Imágenes</p>
-
           <Formulario 
             guardarBusqueda={guardarBusqueda}
           />
       </div>
-
-      <div className="row justify-content-center">
-       
-          <ListadoImagenes 
-            imagenes={imagenes}
-          />
-      
+      <div className="row justify-content-center">       
+        <ListadoImagenes 
+          imagenes={imagenes}
+        />
+          { 
+            paginaactual !== 1 ?
             <button 
                 type="button"
                 className="bbtn btn-info mr-1"
-          
+                onClick={paginaAnterior}
             >&laquo; Anterior </button>
-     
-            <button 
-              type="button"
-              className="bbtn btn-info"
-        
-            >Siguiente &raquo;</button>
-   
+            :
+            null
+          }
+          { 
+            paginaactual !== totalpaginas + 1 && busqueda ?
+              <button 
+                type="button"
+                className="bbtn btn-info"
+                onClick={paginaSiguiente}
+              >Siguiente &raquo;</button>
+            :
+            null
+          }   
       </div>
     </div>
   );
